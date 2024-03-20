96850302b04aca6286b9bdb7e0c8cdafc7a05123
SSeonghun <157373703+SSeonghun@users.noreply.github.com>
2024-03-20 13:40:23 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 440 ms, Memory: 236300 KB -BaekjoonHub

---
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.py
@@ -0,0 +1,15 @@
+s1 = input()
+s2 = input()
+
+def lcs(s1, s2):
+    len1, len2 = len(s1), len(s2)
+    arr = [[0]*(len2+1) for _ in range(len1+1)]
+    Max = 0
+    for i in range(1, len1+1):
+        for j in range(1, len2+1):
+            if s1[i-1]==s2[j-1]:
+                arr[i][j] = arr[i-1][j-1]+1
+                Max = max(Max, arr[i][j])
+    return Max
+
+print(lcs(s1,s2))
