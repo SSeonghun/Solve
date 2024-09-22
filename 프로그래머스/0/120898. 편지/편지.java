5f9ccb634a4427cf0938e7e1ffdccd78ae58aaa9
SSeonghun <157373703+SSeonghun@users.noreply.github.com>
2024-09-23 08:39:20 +0900

    [level 0] Title: 편지, Time: 0.22 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        String[] arr = message.split("");
+        
+        answer = arr.length*2;
+        
+        return answer;
+    }
+}
